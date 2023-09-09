226d8e46ac064500b9eb06414c4a9f85cbbc5ba1
Thando Somacele <tsomacele@gmail.com>
2023-09-09 09:54:38 +0200

    button styles & font styles added

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className='navbar h-20 bg-schemes-light-primaryContainer flex items-center text-schemes-light-onPrimaryContainer'>
+    <header className='navbar h-20 bg-light-background flex items-center text-light-onPrimaryContainer'>
       <div className='container flex'>
-        <div className='navbar-left flex  items-center w-1/2 gap-5'>
+        <div className='navbar-left flex items-center w-1/2 gap-5'>
           <Image className='logo' src='/logo.png' alt='logo' width={181} height={41} priority />
-          <ul className='nav-links flex gap-4'>
+          <ul className='nav-links flex gap-4 text__body-large font-medium'>
             <li className='nav-link'>
               <Link href={'/#features'}>Features</Link>
             </li>
@@ -20,11 +20,11 @@ const Header = () => {
           </ul>
         </div>
         <div className='navbar-right flex  w-1/2 gap-5 justify-end'>
-          <button>
+          <button className='btn btn-primary__outlined text__body-large'>
             <Link href={'#'} />
             Login
           </button>
-          <button>
+          <button className='btn btn-secondary text__body-large'>
             <Link href={'#'} />
             Book Your Cleaner
           </button>
