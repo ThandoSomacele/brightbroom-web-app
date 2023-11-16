b5f2fba05355c3eaccfd622ddb7c957096d4eb3f
Thando Somacele <tsomacele@gmail.com>
2023-11-16 19:36:25 +0200

    Service Card complete

--- src/app/components/parts/ServiceCard.tsx
+++ src/app/components/parts/ServiceCard.tsx
@@ -2,60 +2,67 @@ import Image from 'next/image';
 import React from 'react';
 import CounterInput from '../ui/CounterInput';
 
-function ServiceCard() {
+interface ServiceObject {
+  name: String;
+  description: String;
+  duration: Number;
+  icon: String;
+  price: Number;
+}
+
+function ServiceCard({ service }: { service: ServiceObject }) {
+  const ClockIcon = function () {
+    return (
+      <React.Fragment>
+        <Image src={'/icons/clock.svg'} width={24} height={24} alt='clock icon' />
+
+        <p className='text-xs font-medium'>
+          {`approx. ${service.duration} ${Number(service.duration) > 1 ? 'hours' : 'hour'}`}
+          <sup>*</sup>
+        </p>
+      </React.Fragment>
+    );
+  };
+
   return (
     <div className='rounded-xl bg-white p-4 ring ring-light-primary sm:p-6 lg:p-8'>
-      <div className='flex items-center sm:gap-8'>
+      <div className='flex flex-col md:flex-row items-center sm:gap-8'>
         <div
-          className='service-icon hidden sm:grid sm:h-20 sm:w-20 sm:shrink-0 sm:place-content-center sm:rounded-full sm:border-2 sm:border-light-primary w-1/6'
+          className='service-icon sm:grid sm:h-20 sm:w-20 sm:shrink-0 sm:place-content-center sm:rounded-full sm:border-2 sm:border-light-primary w-full'
           aria-hidden='true'>
           <div className='flex items-center gap-1'>
             <Image
-              src='/booking-services/bedroom.webp'
+              src={`/icons/${service.icon}.svg`}
               className='object-contain'
               width={'45'}
               height={'45'}
-              alt='bedroom icon'
+              alt={`${service.name} icon`}
             />
           </div>
         </div>
 
-        <div className='service-description w-4/6'>
+        <div className='service-description w-full md:w-4/6'>
           <h3 className='text-lg font-medium sm:text-xl'>
             <a href='' className='hover:underline'>
-              Bedroom
+              {service.name}
             </a>
           </h3>
 
-          <p className='mt-1 text-sm text-gray-700'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nulla amet voluptatum sit rerum, atque, quo
-            culpa ut necessitatibus eius suscipit eum accusamus, aperiam voluptas exercitationem facere aliquid fuga.
-            Sint.
-          </p>
+          <p className='mt-1.5 text-sm text-gray-700'>{service.description}</p>
 
           <div className='mt-4 sm:flex sm:items-center sm:gap-2'>
-            <div className='flex items-center gap-1 text-gray-500'>
-              <svg
-                className='h-4 w-4'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'>
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z'></path>
-              </svg>
-
-              <p className='text-xs font-medium'>
-                approx. 60 minutes<sup>*</sup>
-              </p>
+            <div className='hidden md:flex items-center gap-1 text-gray-500'>
+              <ClockIcon />
             </div>
           </div>
         </div>
 
-        <div className='service-controller flex justify-end w-1/6'>
+        <div className='service-controller flex justify-between md:justify-end w-full md:w-1/6'>
+          <div className='mt-4 sm:flex sm:items-center sm:gap-2 md:hidden'>
+            <div className='flex items-center gap-1 text-gray-500'>
+              <ClockIcon />
+            </div>
+          </div>
           <CounterInput />
         </div>
       </div>
--- src/app/components/ui/CounterInput.tsx
+++ src/app/components/ui/CounterInput.tsx
@@ -3,23 +3,23 @@ import React from 'react';
 function CounterInput() {
   return (
     <div className='custom-number-input w-32'>
-      <label htmlFor='custom-input-number' className='hidden w-full text-gray-700 text-sm font-semibold'>
+      <label htmlFor='custom-input-number' className='hidden w-full text-light-onPrimaryFixed text-sm font-semibold'>
         Counter Input
       </label>
       <div className='flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1'>
         <button
           data-action='decrement'
-          className=' bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none'>
+          className='  bg-light-primaryFixed text-light-onPrimaryFixed hover:text-light-onPrimaryFixed hover:bg-light-primaryFixedDim h-full w-20 rounded-l cursor-pointer outline-none'>
           <span className='m-auto text-2xl font-thin'>−</span>
         </button>
         <input
           type='number'
-          className='outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700 outline-none'
+          className='outline-none focus:outline-none text-center w-full  bg-light-primaryFixed text-light-onPrimaryFixed text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center'
           name='custom-input-number'
           value='0'></input>
         <button
           data-action='increment'
-          className='bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer'>
+          className=' bg-light-primaryFixed text-light-onPrimaryFixed hover:text-light-onPrimaryFixed hover:bg-light-primaryFixedDim h-full w-20 rounded-r cursor-pointer'>
           <span className='m-auto text-2xl font-thin'>+</span>
         </button>
       </div>
