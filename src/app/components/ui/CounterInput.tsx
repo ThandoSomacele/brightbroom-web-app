bae5894756606042833851ff6072a6095d452d3b
Thando Somacele <tsomacele@gmail.com>
2023-12-31 15:16:19 +0200

    service card updated to grey out or to input type

--- src/app/components/ui/CounterInput.tsx
+++ src/app/components/ui/CounterInput.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-function CounterInput() {
+interface ServiceObject {
+  name: String;
+  description: String;
+  duration: Number;
+  icon: String;
+  price: Number;
+  input_type: String;
+  isDisabled: Boolean;
+}
+
+function CounterInput({ service }: { service: ServiceObject }) {
   return (
     <div className='custom-number-input w-32'>
       <label htmlFor='custom-input-number' className='hidden w-full text-light-onPrimaryFixed text-sm font-semibold'>
@@ -9,17 +19,30 @@ function CounterInput() {
       <div className='flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1'>
         <button
           data-action='decrement'
-          className='  bg-light-primaryFixed text-light-onPrimaryFixed hover:text-light-onPrimaryFixed hover:bg-light-primaryFixedDim h-full w-20 rounded-l cursor-pointer outline-none'>
+          className={`${service.isDisabled ? 'bg-light-surfaceDim' : 'bg-light-primaryFixed'} ${
+            service.isDisabled ? 'text-light-surfaceDim' : 'text-light-onPrimaryFixed'
+          } hover:text-light-onPrimaryFixed ${
+            service.isDisabled ? 'bg-light-surfaceDim' : 'hover:bg-light-primaryFixedDim'
+          }  h-full w-20 rounded-l ${service.isDisabled ? 'cursor-default' : 'cursor-pointer'} outline-none `}>
           <span className='m-auto text-2xl font-thin'>−</span>
         </button>
         <input
           type='number'
-          className='outline-none focus:outline-none text-center w-full  bg-light-primaryFixed text-light-onPrimaryFixed text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center'
+          className={`outline-none focus:outline-none text-center w-full ${
+            service.isDisabled ? 'bg-light-surfaceDim' : 'bg-light-primaryFixed'
+          } ${
+            service.isDisabled ? 'text-light-surfaceDim' : 'text-light-onPrimaryFixed'
+          } text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center`}
           name='custom-input-number'
-          value='0'></input>
+          defaultValue='1'
+          disabled={service.isDisabled ? true : false}></input>
         <button
           data-action='increment'
-          className=' bg-light-primaryFixed text-light-onPrimaryFixed hover:text-light-onPrimaryFixed hover:bg-light-primaryFixedDim h-full w-20 rounded-r cursor-pointer'>
+          className={`${service.isDisabled ? 'bg-light-surfaceDim' : 'bg-light-primaryFixed'} ${
+            service.isDisabled ? 'text-light-surfaceDim' : 'text-light-onPrimaryFixed'
+          } hover:text-light-onPrimaryFixed ${
+            service.isDisabled ? 'bg-light-surfaceDim' : 'hover:bg-light-primaryFixedDim'
+          } h-full w-20 rounded-r ${service.isDisabled ? 'cursor-default' : 'cursor-pointer'}`}>
           <span className='m-auto text-2xl font-thin'>+</span>
         </button>
       </div>
