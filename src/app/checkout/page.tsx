0cf9935c3f5d51f937bff96422dc8ec82a36a6b4
Thando Somacele <tsomacele@gmail.com>
2024-01-07 19:10:37 +0200

    checkout page and login if wip

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import CheckoutProgressBar from '../components/parts/CheckoutProgressBar';
 import ProvidedCleaner from '../components/parts/ProvidedCleaner';
-import BookingSummary from '../components/parts/BookingSummary';
 import CardPayment from '../components/parts/CardPayment';
+import BookedItems from '../components/parts/BookedItems';
 
 function page() {
   return (
     <div>
       <CheckoutProgressBar />
-      <div className='grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32'>
-        <BookingSummary>
+      <div className='container grid lg:grid-cols-2'>
+        <div className='px-4 pt-8'>
+          <p className='text-xl font-medium'>Booking Summary</p>
+          <p className='text-gray-400'>Check your items before proceeding to make payment.</p>
+          <div className='mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6'>
+            <BookedItems />
+          </div>
           <ProvidedCleaner />
-        </BookingSummary>
+        </div>
         <CardPayment />
       </div>
     </div>
