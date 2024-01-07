d5d50b0d1db645cdee983fa4d6d48807bbdc9e63
Thando Somacele <tsomacele@gmail.com>
2024-01-07 19:10:37 +0200

    checkout page and login if wip

--- src/app/components/parts/CardPayment.tsx
+++ src/app/components/parts/CardPayment.tsx
@@ -135,19 +135,21 @@ function CardPayment() {
         <div className='mt-6 border-t border-b py-2'>
           <div className='flex items-center justify-between'>
             <p className='text-sm font-medium text-gray-900'>Subtotal</p>
-            <p className='font-semibold text-gray-900'>$399.00</p>
+            <p className='font-semibold text-gray-900'>ZAR 399.00</p>
           </div>
           <div className='flex items-center justify-between'>
             <p className='text-sm font-medium text-gray-900'>Shipping</p>
-            <p className='font-semibold text-gray-900'>$8.00</p>
+            <p className='font-semibold text-gray-900'>ZAR 8.00</p>
           </div>
         </div>
         <div className='mt-6 flex items-center justify-between'>
           <p className='text-sm font-medium text-gray-900'>Total</p>
-          <p className='text-2xl font-semibold text-gray-900'>$408.00</p>
+          <p className='text-2xl font-semibold text-gray-900'>ZAR 408.00</p>
         </div>
       </div>
-      <button className='mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white'>Place Order</button>
+      <button className='btn btn-primary mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white'>
+        Make Payment
+      </button>
     </div>
   );
 }
