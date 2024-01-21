958622875a33c2d9911d65bdffe2f8bfce94e8b5
Thando Somacele <tsomacele@gmail.com>
2024-01-21 07:27:37 +0200

    api test working

--- src/app/components/parts/BookBtn.tsx
+++ src/app/components/parts/BookBtn.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import { ClickEvent } from '../../../../types';
 
+// Fetch Fn
+async function getData() {
+  const res = await fetch('/api/createBooking');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.text();
+}
+
+// Component
 function BookBtn() {
+  const actionHandler = async (e: ClickEvent) => {
+    e.preventDefault();
+    console.log(' coming...');
+    const data = await getData();
+    console.log(data);
+  };
+
   return (
-    <button type='submit' className='btn btn-primary w-full fixed bottom-0 right-0 rounded-none'>
-      R {price} • {totalDuration} Hours
+    <button
+      type='submit'
+      onClick={actionHandler}
+      className='btn btn-primary w-full fixed bottom-0 right-0 rounded-none'>
+      Book a Cleaner
     </button>
   );
 }
--- src/app/components/parts/ToggleSwitch.tsx
+++ src/app/components/parts/ToggleSwitch.tsx
@@ -5,48 +5,52 @@ function ToggleSwitch({
   service,
   price,
   setPrice,
-  totalDuration,
-  setTotalDuration,
+  totalHours,
+  setTotalHours,
 }: {
   service: ServiceObject;
   price: number;
   setPrice: Dispatch<SetStateAction<number>>;
-  totalDuration: number;
-  setTotalDuration: Dispatch<SetStateAction<number>>;
+  totalHours: number;
+  setTotalHours: Dispatch<SetStateAction<number>>;
 }) {
   const [isChecked, setChecked] = useState(false);
 
   const addonSwitchHandler = (e: ChangeEvent) => {
     e.preventDefault();
-    if (totalDuration + service.duration > 10) {
+    // TODO
+    if (totalHours + service.hours > 10) {
       alert('Maximum hours is 10. \nAdjust to prioritise needed services.');
-      return;
     }
 
-    if (isChecked === false && totalDuration + service.duration <= 10) {
+    if (isChecked === false && totalHours + service.hours <= 10) {
       setChecked(true);
       setPrice((price += service.price));
-      setTotalDuration((totalDuration += service.duration));
-      return;
+      setTotalHours((totalHours += service.hours));
     }
 
-    if (totalDuration - service.duration < 4) {
+    if (totalHours - service.hours < 4) {
       alert('Minimum hours is 4. \nAdjust to prioritise needed services.');
-      return;
     }
 
-    if (isChecked === true && totalDuration >= 4) {
+    if (isChecked === true && totalHours >= 4) {
       setChecked(false);
       setPrice(price - service.price);
-      setTotalDuration((totalDuration -= service.duration));
-      return;
+      setTotalHours((totalHours -= service.hours));
     }
   };
 
   return (
     <div>
       <label className='relative inline-flex items-center cursor-pointer'>
-        <input type='checkbox' className='sr-only peer' onChange={addonSwitchHandler} checked={isChecked} />
+        <input
+          type='checkbox'
+          className='sr-only peer'
+          onChange={addonSwitchHandler}
+          name={service.name}
+          value='1'
+          checked={isChecked}
+        />
         <div className="w-11 h-6 bg-light-surfaceVariant peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-light-onPrimaryContainer rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-light-onPrimary after:bg-light-surfaceVarian after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-light-primaryContainer"></div>
       </label>
     </div>
