0832899166bf9b8cd6e1cff075d24a00a7be1b7f
Thando Somacele <tsomacele@gmail.com>
2024-01-25 15:30:21 +0200

    formDatat working in booking page

--- src/app/components/parts/ToggleSwitch.tsx
+++ src/app/components/parts/ToggleSwitch.tsx
@@ -41,19 +41,49 @@ function ToggleSwitch({
   };
 
   return (
-    <div>
-      <label className='relative inline-flex items-center cursor-pointer'>
-        <input
-          type='checkbox'
-          className='sr-only peer'
-          onChange={addonSwitchHandler}
-          name={service.name}
-          value='1'
-          checked={isChecked}
-        />
-        <div className="w-11 h-6 bg-light-surfaceVariant peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-light-onPrimaryContainer rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-light-onPrimary after:bg-light-surfaceVarian after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-light-primaryContainer"></div>
-      </label>
-    </div>
+    <label
+      htmlFor={service.name}
+      className='relative h-8 w-14 cursor-pointer [-webkit-tap-highlight-color:_transparent]'>
+      <input
+        type='checkbox'
+        onChange={addonSwitchHandler}
+        id={service.name}
+        name={service.name}
+        value='Added'
+        defaultChecked={false}
+        className='peer sr-only [&:checked_+_span_svg[data-checked-icon]]:block [&:checked_+_span_svg[data-unchecked-icon]]:hidden'
+      />
+
+      <span className='absolute inset-y-0 start-0 z-10 m-1 inline-flex h-6 w-6 items-center justify-center rounded-full bg-light-onPrimary text-palettes-neutralVariant-80 transition-all peer-checked:start-6 peer-checked:text-light-primary'>
+        <svg
+          data-unchecked-icon
+          xmlns='http://www.w3.org/2000/svg'
+          className='h-4 w-4'
+          viewBox='0 0 20 20'
+          fill='currentColor'>
+          <path
+            fill-rule='evenodd'
+            d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+            clip-rule='evenodd'
+          />
+        </svg>
+
+        <svg
+          data-checked-icon
+          xmlns='http://www.w3.org/2000/svg'
+          className='hidden h-4 w-4'
+          viewBox='0 0 20 20'
+          fill='currentColor'>
+          <path
+            fill-rule='evenodd'
+            d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+            clip-rule='evenodd'
+          />
+        </svg>
+      </span>
+
+      <span className='absolute inset-0 rounded-full bg-palettes-neutralVariant-80 transition peer-checked:bg-light-primaryContainer'></span>
+    </label>
   );
 }
 
