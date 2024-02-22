ec12c12ab6c9f67c2574909b41036570c455875f
Thando Somacele <tsomacele@gmail.com>
2024-02-22 18:50:01 +0200

    dockerfile not needed for nextjs apps

--- app/ui/home/OurFeatures.tsx
+++ app/ui/home/OurFeatures.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
-import CtaLinks from '../misc/CtaLinks';
-import Features from './Features';
+import CtaLinks from '@/ui/misc/CtaLinks';
+import Features from '@/ui/home/Features';
 
 const OurFeatures = () => {
   return (
-    <section id='features' className='py-16 bg-palettes-primary-99'>
-      <div className='container flex flex-wrap justify-between gap-4'>
-        <div className='our-features-left w-full lg:w-[40%] flex flex-col gap-6'>
-          <h2 className='text__display-small md:text__display-medium'>
+    <section id="features" className="bg-palettes-primary-99 py-16">
+      <div className="container flex flex-wrap justify-between gap-4">
+        <div className="our-features-left flex w-full flex-col gap-6 lg:w-[40%]">
+          <h2 className="text__display-small md:text__display-medium">
             Our Features
             <br />& Services
           </h2>
@@ -18,11 +18,11 @@ const OurFeatures = () => {
             alt={'Clean bedroom with open window'}
             width={404}
             height={277}
-            className='w-full md:h-80 lg:h-full object-cover rounded-2xl'
+            className="w-full rounded-2xl object-cover md:h-80 lg:h-full"
             priority
           />
         </div>
-        <div className='how-it-work-right w-full lg:w-[53%]'>
+        <div className="how-it-work-right w-full lg:w-[53%]">
           <Features />
         </div>
       </div>
