aff03b78b210801c301f6b6cce8e1470acca45a0
Thando Somacele <tsomacele@gmail.com>
2024-02-26 16:14:31 +0200

    dasboard customers added

--- app/(cleaners-pages)/become-a-cleaner/page.tsx
+++ app/(cleaners-pages)/become-a-cleaner/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const BecomeACleaner = () => {
-  return <div>BecomeACleaner</div>;
+  return (
+    <iframe
+      // className="container"
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfj-hmBMxqYtWwkNhbMwpS3qqyVMqr94u29YPtk2haYzNXLDg/viewform?embedded=true"
+      width="100%"
+      height="3309"
+    >
+      Loading…
+    </iframe>
+  );
 };
 
 export default BecomeACleaner;
