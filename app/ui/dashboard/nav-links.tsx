30ca1ce804fa7f025fabb993f5e9564440bbd83c
Thando Somacele <thando.somacele@gmail.com>
2024-04-01 11:18:21 +0200

    wip editing invoices

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -2,6 +2,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  GiftIcon,
 } from '@heroicons/react/24/outline';
 
 // Map of links to display in the side navigation.
@@ -14,6 +15,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Bookings', href: '/dashboard/bookings', icon: GiftIcon },
 ];
 
 export default function NavLinks() {
